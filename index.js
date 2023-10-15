593327af9b8a5c544d3142d21ed7fe8ec7ba6dea
Jocelyn-MB <mancillablancasjocelyn@gmail.com>
2023-10-15 00:30:18 -0600

    primer commit

---
+++ index.js
@@ -0,0 +1,30 @@
+var express=require("express");
+var path= require("path");
+var cors = require("cors");
+var rutasUsuarios=require("./rutas/usuariosRutas"); 
+var rutasProductos=require ("./rutas/productosRutas");
+var rutasUsuariosApis=require("./rutas/usuariosRutasApis"); 
+var rutasProductosApis = require ("./rutas/productosRutasApis");
+var app=express();
+
+require("dotenv").config();
+app.set("view engine","ejs"); 
+app.use(cors())
+app.use(express.json()); 
+app.use(express.urlencoded({extended:true})); //cuando esta en false quiere decir que no voy a podermandar datos 
+app.use("/",express.static(path.join(__dirname,"/web")));
+app.use("/",rutasUsuarios);
+app.use("/",rutasProductos);
+app.use("/",rutasUsuariosApis);
+app.use("/",rutasProductosApis);
+
+app.use(session({
+    secret:process.env.SESSION_SECRETO,
+    resave:true,
+    saveUninitialized:true
+}));
+var port=process.env.PORT || 3000; 
+
+app.listen(port, ()=>{
+    console.log("server in http://localhost:"+port);
+});
---
+++ rutas/productosRutas.js
@@ -0,0 +1,68 @@
+var rutaP = require("express").Router();
+var subirArchivo = require ("../middlewares/middlewares").subirArchivo;
+var {mostrarProductos, nuevoProducto, buscarPorIdP, modificarProducto,borrarProducto} = require("../bd/productosBD");
+var fs = require("fs");
+var path= require("path");
+
+rutaP.get("/mostrarProductos", async (req, res) => {
+    var prods = await mostrarProductos();
+    //console.log(users);
+    res.render("productos/mostrar", {prods});
+})
+rutaP.get("/nuevoProducto",(req,res)=>{
+    res.render("productos/nuevo");
+}); 
+
+rutaP.post("/nuevoProducto",subirArchivo(),async (req,res)=>{
+    //console.log(req.body);
+    if (req.file) {
+        req.body.foto = req.file.originalname;
+        var error= await nuevoProducto(req.body);
+    }
+    //req.body.foto= req.file.originalname;
+    //var error= await nuevoProducto(req.body);
+    console.log(error);
+    res.redirect("/mostrarProductos"); 
+}); 
+
+rutaP.get("/editarProducto/:id",async(req,res)=>{
+    var prod= await buscarPorIdP(req.params.id);
+    res.render("productos/modificar",{prod});
+});
+
+rutaP.post("/editarProducto",subirArchivo(), async (req,res)=>{
+   try {
+        
+            var rutaimg = path.join(__dirname, "..", "web", "images", req.body.foto);
+            if (fs.existsSync(rutaimg)) {
+                fs.unlinkSync(rutaimg);
+                req.body.foto= req.file.originalname;
+                await modificarProducto(req.body);
+            }
+        
+        res.redirect("/mostrarProductos");
+    } catch (error) {
+        console.error("Error al editar usuario:", error);
+    }
+    /*
+
+var error=  await modificarProducto(req.body);
+    res.redirect("/mostrarProductos");*/
+});
+
+rutaP.get("/borrarProducto/:id", async (req,res)=>{
+    var usuario = await buscarPorIdP(req.params.id);
+        if (usuario) {
+            var rutaimg = path.join(__dirname, "../web/images", usuario.foto);
+            if (fs.existsSync(rutaimg)) {
+                fs.unlinkSync(rutaimg);
+            }
+            await borrarProducto(req.params.id);
+        }
+        res.redirect("/mostrarProductos");
+
+    /*await borrarProducto(req.params.id);
+    res.redirect("/mostrarProductos");*/
+})
+
+module.exports = rutaP;
